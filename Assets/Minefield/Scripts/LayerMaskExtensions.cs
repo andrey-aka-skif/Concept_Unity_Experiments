1e9a6a7c20be3f2f6065e08768d46b7b31740163
andrey-aka-skif <2119163@gmail.com>
2023-01-05 00:11:44 +0300

    Расширение для сравнения слоёв

---
+++ Assets/Minefield/Scripts/LayerMaskExtensions.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Minefield.Extensions
+{
+    public static class LayerMaskExtensions
+    {
+        public static bool Includes(this LayerMask mask, int layer)
+        {
+            return (mask.value & 1 << layer) > 0;
+        }
+    }
+}
